f1a80b8ff1c9293b97ae275776122267df5eef37
Samira <54632872+S-samira2020@users.noreply.github.com>
2020-09-22 00:26:31 +0600

    Add files via upload

---
+++ numeric.py
@@ -0,0 +1,24 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age  = 18
+>>> age  == 18
+True
+>>> age  = 17
+>>> age == 18
+False
+>>> age != 18
+True
+>>> age  ! = 18
+SyntaxError: invalid syntax
+>>> age != 18
+True
+>>> age  = 19
+>>> age < 21
+True
+>>> age > 21
+False
+>>> age <= 21
+True
+>>> age >= 21
+False
+>>> 
